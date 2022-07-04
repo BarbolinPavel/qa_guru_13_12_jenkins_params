7bcdfd04827266e9d5b11478c1fdf9f2e1198d8f
Pavel <frost1449@gmail.com>
2022-07-04 05:36:40 +0300

    update TestBase

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -21,12 +21,14 @@ public class TestBase {
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
 
-        String selenideUrl = System.getProperty("selenideUrl", "selenoid.autotests.cloud/wd/hub");
+        String selenideUrl = System.getProperty("selenideUrl");
 
         Configuration.browserCapabilities = capabilities;
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        Configuration.remote = "https://user1:1234@" + selenideUrl;
+        //Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
     @AfterEach
