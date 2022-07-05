053c96fd5cf6cae1d3041e88b544f00fb06f5073
Pavel <frost1449@gmail.com>
2022-07-05 05:53:37 +0300

    update TestBase

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,7 +2,9 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import config.CredentialsConfig;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -12,23 +14,27 @@ import pages.components.Attach;
 public class TestBase {
 
     RegistrationFormPage registrationFormPage = new RegistrationFormPage();
+    public static CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
 
     @BeforeAll
     static void setUp() {
+
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        String login = config.login();
+        String password = config.password();
+        String selenoidUrl = System.getProperty("selenoidUrl");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
 
-        String selenoidUrl = System.getProperty("selenoidUrl");
-
         Configuration.browserCapabilities = capabilities;
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
-
-        Configuration.remote = "https://user1:1234" + selenoidUrl;
-        //Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.browser = System.getProperty("browser","chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion","102");
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+        Configuration.remote = String.format("https://%s:%s@%s", login, password, selenoidUrl);
     }
 
     @AfterEach
